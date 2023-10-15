7c051bbd036f840172380b8e28af39d122bdb2a4
ImmanuelRaghu <147996079+ImmanuelRaghu@users.noreply.github.com>
2023-10-15 21:16:11 +0530

    Add files via upload

---
+++ Powershell Assessment.ps1
@@ -0,0 +1,51 @@
+﻿function Get-CommonTimeZone {
+    [CmdletBinding()]
+    param(
+        [string]$Name,
+        [int]$Offset
+    )
+
+    # Ensure that both 'Name' and 'Offset' parameters are not supplied simultaneously
+    if ($Name -and $Offset) {
+        Write-Error "You cannot supply both 'Name' and 'Offset' parameters simultaneously."
+        return $null
+    }
+
+    # Validate the 'Offset' parameter
+    if ($Offset -and ($Offset -lt -12 -or $Offset -gt 12)) {
+        Write-Error "Invalid offset. Offset should be between -12 and 12."
+        return $null
+    }
+
+    # Define the URL to the JSON file containing time zones
+    $jsonUrl = "https://raw.githubusercontent.com/dmfilipenko/timezones.json/master/timezones.json"
+
+    try {
+        # Download the JSON data from the URL
+        $timeZonesData = Invoke-RestMethod -Uri $jsonUrl -UseBasicParsing -ErrorAction Stop
+        
+        # convett from Json data into pharse
+        $timeZonesDatanew =$timeZonesData | ConvertFrom-Json
+
+        # Filter time zones by name if 'Name' parameter is provided
+        if ($Name) {
+            $timeZonesDatanew = $timeZonesData | Where-Object { $_.Name -like "*$Name*" }
+        }
+
+        # Filter time zones by offset if 'Offset' parameter is provided
+        if ($Offset) {
+            $timeZonesDatanew = $timeZonesData | Where-Object { $_.Offset -eq $Offset }
+        }
+
+        $timeZonesDatanew
+    }
+    catch {
+        Write-Error "Failed to retrieve time zone data from GitHub. Please check your internet connection."
+        return $null
+    }
+}
+
+$timeZonesDatanew = Get-CommonTimeZone
+
+
+# NB: the git hub link which is provided is not working to test the script  
